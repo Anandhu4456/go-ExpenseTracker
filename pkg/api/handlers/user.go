c0fb5e2c7b67c99547ea8da725b1ab0868642300
Anandhu4456 <anandhurajan56@gmail.com>
2024-01-09 15:29:27 +0530

    added handler

--- pkg/api/handlers/user.go
+++ pkg/api/handlers/user.go
@@ -1,1 +1,35 @@
 package handlers
+
+import (
+	"go-Expense/pkg/models"
+	"go-Expense/pkg/response"
+	services "go-Expense/pkg/usecase/interfaces"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserHandler struct {
+	userUsecase services.UserUsecase
+}
+
+func NewUserHandler(userUsecase services.UserUsecase) UserHandler {
+	return UserHandler{
+		userUsecase: userUsecase,
+	}
+}
+func (usrH UserHandler) AddSection(c *gin.Context) {
+	var section models.Section
+	if err := c.BindJSON(section); err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "adding section failed", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+	}
+	add, addErr := usrH.userUsecase.AddSection(section.UserName, section.Buget, section.Description, section.CreatedAt)
+	if addErr != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "adding failed", nil, addErr.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+	success := response.ClientResponse(http.StatusOK, "section added successfully", add, nil)
+	c.JSON(http.StatusOK, success)
+}
--- pkg/response/response.go
+++ pkg/response/response.go
@@ -1,14 +1,15 @@
 package response
 
+
 type Response struct {
 	StatusCode int
 	Message    string
 	Data       interface{}
-	Error      error
+	Error      interface{}
 }
 
-func (r *Response) ClientResponse(statusCode int, message string, data interface{}, err error) *Response {
-	return &Response{
+func ClientResponse(statusCode int, message string, data interface{}, err interface{}) Response {
+	return Response{
 		StatusCode: statusCode,
 		Message:    message,
 		Data:       data,
