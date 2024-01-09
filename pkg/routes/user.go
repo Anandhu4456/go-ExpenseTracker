9aa9d543a5aa213ab06873c1f12d8e609496c1af
Anandhu4456 <anandhurajan56@gmail.com>
2024-01-09 23:27:01 +0530

    added user route

--- pkg/routes/user.go
+++ pkg/routes/user.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	
+	"go-Expense/pkg/api/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(engine *gin.RouterGroup, userHandler handlers.UserHandler){
+
+	forUsers:=engine.Group("/users")
+	{
+		// user routes will go here
+		forUsers.POST("/addsection",userHandler.AddSection)
+	}
+}
