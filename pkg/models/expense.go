302376bd84e3c32b89e10d040e06e064a5816eb4
Anandhu4456 <anandhurajan56@gmail.com>
2024-01-13 11:55:30 +0530

    added delete function in repo

--- pkg/models/expense.go
+++ pkg/models/expense.go
@@ -1,7 +1,9 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Section struct {
-	// ID          primitive.ObjectID
+	ID          primitive.ObjectID `json:"id" bson:"_id"`
 	UserName    string  `json:"user_name"`
 	Buget       float64 `json:"buget"`
 	Description string  `json:"description"`
--- pkg/repository/user.go
+++ pkg/repository/user.go
@@ -6,6 +6,8 @@ import (
 	"go-Expense/pkg/repository/interfaces"
 	"log"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -43,4 +45,21 @@ func (ur UserRepo) AddSection(userName string, buget float64, description string
 	}
 	log.Println("section added with id :", result.InsertedID)
 	return section, nil
+}
+
+func (ur UserRepo) DeleteSection(id string) error {
+	hexId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Println("error while getting hex id ", err)
+		return err
+	}
+	filter := bson.M{"_id": hexId}
+	coll := ur.client.Database("Expense").Collection("section")
+	result, err := coll.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		log.Println("deleting section failed", err)
+		return err
+	}
+	log.Println("section deleted with id ", result.DeletedCount)
+	return nil
 }
